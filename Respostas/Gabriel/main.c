b5b6b72481d46062af2dd02229b91922754ab3b5
gabrielsenadc <gabriel.cunha.74@edu.ufes.br>
2023-12-06 15:44:42 -0300

    vsf

--- Respostas/Gabriel/main.c
+++ Respostas/Gabriel/main.c
@@ -14,10 +14,10 @@ tPessoa* login(tListaPessoas *lista){
     while(acesso == 'E'){
         existe = 0;
         char usuario[20], senha[20];
-        printf("\nDIGITE SEU LOGIN:");
+        printf("\nDIGITE SEU LOGIN: ");
         scanf("%s%*c", usuario);
 
-        printf("DIGITE SUA SENHA:");
+        printf("DIGITE SUA SENHA: ");
         scanf("%s%*c", senha);
 
         for(int i = 0; i < retornaQtdPesssoas(lista); i++){
@@ -38,6 +38,26 @@ tPessoa* login(tListaPessoas *lista){
             acesso = 'E';
         }
     }
+}
+
+void buscaPaciente(tFila *fila, tListaPessoas *listaPessoas){
+    char nome[100];
+    printf("NOME DO PACIENTE: ");
+    scanf("%[\n]%*c", nome);
+    tListaBusca *lista = criaListaBusca();
+    for(int i = 0; i < retornaQtdPesssoas(listaPessoas); i++){
+        if(igualNome(nome, listaPessoas->pessoa[i])){
+            adicionaPessoaListaBusca(lista, listaPessoas->pessoa[i]);
+        }
+    }
+    printf("PACIENTES ENCONTRADOS:\n");
+    imprimeNaTelaListaBusca(lista);
+
+    printf("SELECIONE UMA OPÇÃO:\n(1) ENVIAR LISTA PARA IMPRESSAO\n(2) RETORNAR AO MENU PRINCIPAL");
+    int acao;
+    scanf("%d%*c", &acao);
+    if(acao == 1)
+        insereDocumentoFila(fila, lista, imprimeNaTelaListaBusca, imprimeEmArquivoListaBusca, desalocaListaBusca);
 }
 
 int main(){
@@ -63,6 +83,7 @@ int main(){
     }
 
     int acao;
+    char teste;
     while(1){
         if(retornaCargo(usuario) == 'A'){
             printf("ESCOLHA UMA OPCAO:\n(1) CADASTRAR SECRETARIO\n(2) CADASTRAR MEDICO\n(3) CADASTRAR PACIENTE\n");
@@ -77,13 +98,18 @@ int main(){
             printf("(4) REALIZAR CONSULTA\n(5) BUSCAR PACIENTES\n(6) RELATORIO GERAL\n(7) FILA DE IMPRESSAO\n(8)FINALIZAR O PROGRAMA\n");
         }
         scanf("%d%*c", &acao);
-        if(acao == 1 || acao == 2 || acao == 3)
+        if(acao == 1 || acao == 2 || acao == 3){
             cadastraPessoa(listaPessoas, acao);
+            printf("PRESSIONE QUALQUER TECLA PARA RETORNAR AO MENU ANTERIOR\n");
+            scanf("%c", &teste);
+            printf("%c", teste);
+        }
         if(acao == 4)
             RealizaConsulta(fila, listaPessoas, nomeMedico, CRM);
-        if(acao == 5){
-            
-        }
+        if(acao == 5)
+            buscaPaciente(fila, listaPessoas);
+        if(acao == 6)
+            scanf("%d%*c", &acao);
         if(acao == 7)
             imprimeFila(fila, "saida");
         
@@ -92,9 +118,4 @@ int main(){
     }
 
 
-    /*char nome[100];
-    for(int i = 0; i < retornaQtdPesssoas(listaPessoas); i++){
-        strcpy(nome, retornaNome(listaPessoas->pessoa[i]));
-        printf("%s\n", nome);
-    }*/
 }
