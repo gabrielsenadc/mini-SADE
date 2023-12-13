fb43cff4fa119cb28ad473149af77499a54d4734
gabrielsenadc <gabriel.cunha.74@edu.ufes.br>
2023-12-12 21:31:19 -0300

    final

---
+++ GabrielSenaCunha/tBiopsia.c
@@ -0,0 +1,66 @@
+#include "tBiopsia.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_TAM_NOME 100
+
+struct tBiopsia{
+  char nomePaciente[MAX_TAM_NOME];
+  char CPF[15];
+  tListaLesao *lista;
+  char nomeMedico[MAX_TAM_NOME];
+  char CRM[12];
+  char data[11];
+};
+
+tBiopsia *criaBiopsia(char *nomePaciente, char *CPF, tListaLesao *lista, char *nomeMedico, char *CRM, char *data){
+  tBiopsia *b = malloc(sizeof(tBiopsia));
+  strcpy(b->nomePaciente, nomePaciente);
+  strcpy(b->CPF, CPF);
+  b->lista = lista;
+  strcpy(b->nomeMedico, nomeMedico);
+  strcpy(b->CRM, CRM);
+  strcpy(b->data, data);
+  return b;
+}
+
+void desalocaBiopsia(void *dado){
+  if(dado != NULL){
+    tBiopsia *b = (tBiopsia*) dado;
+    free(b);
+  }
+}
+
+void imprimeNaTelaBiopsia(void *dado){
+  tBiopsia *b = (tBiopsia*) dado;
+  printf("PACIENTE: %s\n", b->nomePaciente);
+  printf("CPF: %s\n\n", b->CPF);
+  printf("SOLICITACAO DE BIOPSIA PARA AS LESOES:\n");
+  for(int i = 0; i < retornaNumeroLesoes(b->lista); i++){
+    if(retornaSeLesaoParaCirurgia(retornaLesaoLista(b->lista, i)))
+      imprimeNaTelaLesao(retornaLesaoLista(b->lista, i));
+  }
+  printf("\n%s (%s)\n", b->nomeMedico, b->CRM);
+  printf("%s\n", b->data);
+}
+
+void imprimeEmArquivoBiopsia(void *dado, char *path){
+  char arquivo[100];
+  sprintf(arquivo, "%s/biopsia.txt", path);
+  FILE *arq = NULL;
+  arq = fopen(arquivo, "a");
+  tBiopsia *b = (tBiopsia*) dado;
+
+  fprintf(arq, "PACIENTE: %s\n", b->nomePaciente);
+  fprintf(arq, "CPF: %s\n\n", b->CPF);
+  fprintf(arq, "SOLICITACAO DE BIOPSIA PARA AS LESOES:\n");
+  for(int i = 0; i < retornaNumeroLesoes(b->lista); i++){
+    if(retornaSeLesaoParaCirurgia(retornaLesaoLista(b->lista, i)))
+      imprimeEmArquivoLesao(retornaLesaoLista(b->lista, i), arq);
+  }
+  fprintf(arq, "\n%s (%s)\n", b->nomeMedico, b->CRM);
+  fprintf(arq, "%s\n\n", b->data);
+
+  fclose(arq);
+}
---
+++ GabrielSenaCunha/tFila.c
@@ -0,0 +1,51 @@
+#include "tFila.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tFila {
+  int qtd;
+  int jaImpresso;
+  tDocumento **documento;
+};
+
+tFila *criaFila(){
+  tFila *fila = malloc(sizeof(tFila));
+  fila->qtd = 0;
+  fila->documento = NULL;
+  fila->jaImpresso = -1;
+  return fila;
+}
+
+void desalocaFila(tFila *f){
+  if(f != NULL){
+    if(f->documento != NULL){
+      for(int i = 0; i < f->qtd; i++){
+        desalocaDocumento(f->documento[i]);
+      }
+      free(f->documento);
+    }
+    free(f);
+  }
+}
+
+void insereDocumentoFila(tFila *f, void *dado, func_ptr_imprimeNaTela imprimeNaTela,
+ func_ptr_imprimeEmArquivo ImprimeEmArquivo,
+ func_ptr_desaloca desaloca){
+  f->qtd++;
+  f->documento = realloc(f->documento, f->qtd * sizeof(tDocumento*));
+  f->documento[f->qtd - 1] = criaDocumento(dado, imprimeNaTela, ImprimeEmArquivo, desaloca);
+ }
+
+int quantidadeDocumentosNaFila(tFila *f){
+  return f->qtd;
+}
+
+void imprimeFila(tFila *f, char *path){
+  for(int i = 0; i < f->qtd; i++){
+    if(i > f->jaImpresso){
+      imprimeNaTelaDocumento(f->documento[i]);
+      imprimeEmArquivoDocumento(f->documento[i], path);
+    }
+  }
+  f->jaImpresso = f->qtd - 1;
+}
